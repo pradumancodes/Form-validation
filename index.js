0e8bf8473e798d2b7f76bae3ec3430d77d642111
Praduman Sharma <154724772+pradumancodes@users.noreply.github.com>
2024-06-20 11:57:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,54 @@
 function clearErrors() {
-    errors = document.getElementsByClassName('formerror');
-    for(let item of errors)
-    {
+    let errors = document.getElementsByClassName('formerror');
+    for (let item of errors) {
         item.innerHTML = "";
     }
+}
 
+function seterror(id, error) {
+    // sets error inside tag of id
+    let element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
+}
+
+function validateform() {
+    let returnval = true;
+    clearErrors();
+
+    // Validate name
+    let name = document.forms['myform']['fname'].value;
+    if (name.length < 5) {
+        seterror('name', '*Name must be at least 5 characters long');
+        returnval = false;
     }
 
-    function seterror(id, error)
-    {
-        //set error 
-        element = document.getElementById(id);
-        element.getElementsByClassName('formerror')[0].innerHTML = error;
+    // Validate email
+    let email = document.forms['myform']['femail'].value;
+    if (email.length > 50) {
+        seterror('email', '*Email length is too long');
+        returnval = false;
     }
 
-    function validateform(){
-        var returnval = true;
-        clearErrors();
-        
-        var name = document.forms['myform']["fname"].value;
-        if(name.length<5){
-            seterror("name", "*Length of name is too short!");
-            returnval = false;
-        }
-
-        if(name.length==0){
-            seterror("name", "*Length of name cannot be zero!");
-            returnval = false;
-        }
-
-        var email = document.forms['myform']["femail"].value;
-        if(email.length>25){
-            seterror("email", "*Email length is too long!");
-            returnval = false;
-        }
-        
-
-        var phone = document.forms['myform']["fphone"].value;
-        if(phone.length!=10){
-            seterror("phone", "*Phone Number should be 10 digits!");
-            returnval = false;
-        }
-
-        var password = document.forms['myform']["fpass"].value;
-        if(password.length < 6){
-            seterror("pass", "*Password should be atleast 6 character long!");
-            returnval = false;
-        }
-
-
-        var cpassword = document.forms['myform']["fcpass"].value;
-        if(cpassword.length < 6){
-            seterror("cpass", "*Password and Confirm Password should match!");
-            returnval = false;
-        }
-        
-        return returnval;
-
+    // Validate phone
+    let phone = document.forms['myform']['fphone'].value;
+    if (phone.length != 10) {
+        seterror('phone', '*Phone number should be of 10 digits');
+        returnval = false;
     }
+
+    // Validate password
+    let password = document.forms['myform']['fpass'].value;
+    if (password.length < 6) {
+        seterror('pass', '*Password should be at least 6 characters long');
+        returnval = false;
+    }
+
+    // Validate confirm password
+    let cpassword = document.forms['myform']['fcpass'].value;
+    if (cpassword != password) {
+        seterror('cpass', '*Passwords do not match');
+        returnval = false;
+    }
+
+    return returnval;
+}
